fbb05a9d29c5a9088cd8333f051327ecfcb1a522
nico-ghub <119863021+nico-ghub@users.noreply.github.com>
2022-12-12 18:29:09 +0100

    Update main.py

--- day12/main.py
+++ day12/main.py
@@ -1,11 +1,11 @@
 def solve( file, p = 1 ):
     d=[]
-    s_l=set()
+    l=set()
     for i, line in enumerate( open( file ) ):
         d.append( [] )
         for j, c in enumerate( line.strip() ):
             if c == "S":
-                s_l.add((i, j))
+                l.add( (i, j) )
                 c="a"
             if c == "E":
                 e = i, j
@@ -19,39 +19,24 @@ def solve( file, p = 1 ):
         for j in range( w ):
             #on ajoute les bords de niveau 0 pour la partie 2
             if ( p==2) and ( i in (0, h-1) or j in (0,w-1) ) and d[i][j] == 0:
-                s_l.add( ( i, j ) )
-            
+                l.add( (i, j) )
             g[i,j] = []
             for di, dj in (-1,0), (1,0), (0,-1), (0,1):
                 i2, j2 = i+di, j+dj
                 if 0<= i2 < h and 0<= j2 < w and d[i][j] + 1 >= d[i2][j2]:
                     g[i,j].append( (i2,j2) )
 
-    m=[]
-    for i, s in enumerate( s_l ):
-        print( i+1, "/", len( s_l) )
-        ds=dict( )
-        for i in range( h ):
-            for j in range( w ):
-                if (i, j) == s:
-                    ds[i,j] = 0
-                elif (i, j) in g[s]:
-                    ds[i,j] = 1
-                else:
-                    ds[i,j] = h*w
-
-        updated=True
-        while updated:
-            updated=False
-            for i in ds:
-                for j in g[i]:
-                    if ds[j] > ds[i] + 1:
-                        ds[j] = ds[i] + 1
-                        updated=True
-        
-        m.append( ds[e] )
-        
-    print( min( m ) )
+    ds=dict( (i, 0 ) for i in l )
+    while len( l )>0:
+        l2=set()
+        for i in l:
+            for j in g[i]:
+                ds.setdefault( j, h*w )
+                if ds[j] > ds[i] + 1:
+                    ds[j] = ds[i] + 1
+                    l2.add( j )
+        l=l2
+    print( ds[e] )
 
 
 solve( "input_ex", 1 )
