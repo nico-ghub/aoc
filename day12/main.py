40a96d5b847110bcc64eaccc73ced9d4fd696dde
nico-ghub <119863021+nico-ghub@users.noreply.github.com>
2022-12-12 11:10:45 +0100

    Create main.py

--- day12/main.py
+++ day12/main.py
@@ -15,29 +15,28 @@ def solve( file ):
     g={}
     for i in range( h ):
         for j in range( w ):
-            g[i,j]={}
+            g[i,j]={(i,j):0}
             for di, dj in (-1,0), (1,0), (0,-1), (0,1):
                 i2, j2 = i+di, j+dj
-                if 0<= i2 < h and 0<= j2 < w and d[i][j] <=  d[i2][j2]:
+                if 0<= i2 < h and 0<= j2 < w and d[i][j] + 1 >= d[i2][j2]:
                     g[i,j][i2,j2]=1
-    
 
     updated=True
+    i=0
     while updated:
         new=[]
         for n, l in g.items():
             for n2, w in l.items():
-                for n3, w2 in g[n2]:
-                    if l.get( n3, h*w ) < w+w2:
+                for n3, w2 in g[n2].items():
+                    if w+w2 < l.get( n3, h*w ):
                         new.append( ( n, n3, w+w2 ) )
         for n, n3, w in new:
-            g[n][n2]=w
+            g[n][n3]=w
         updated = len( new ) > 0
+        i+=1
+        print( i, len( new ) )
     
-    print( g )
-        
+    print( g[s][e] )
 
 solve( "input_ex" )
-#solve( "input"  )
-
-
+solve( "input"  )
