302d41840543cc7e0bbe47828b7891574ad28652
nico-ghub <119863021+nico-ghub@users.noreply.github.com>
2024-12-06 15:16:38 +0100

    part2

--- 2024/d6/main.py
+++ 2024/d6/main.py
@@ -1,53 +1,63 @@
 file="example.txt"
-# file="input.txt"
+file="input.txt"
 
-d=( (0,-1 ), ( 1,0 ), ( 0, 1 ), ( -1, 0 ) )
-i=0
-
-g=[]
-for y, line in enumerate( open( file ) ):
-    l = []
-    g.append( l )
-    for x, c in enumerate( line.strip() ):
+walls=set()
+for h, line in enumerate( open( file ) ):
+    for w, c in enumerate( line.strip() ):
         if c == "^":
-            start = x,y
-            c="X"
-        l.append( [ c=="#", set() ] )
+            start = w,h
+        elif c == "#":
+            walls.add( (w,h) )
+h+=1
+w+=1
 
 #part1
-x,y=start
-o=set()
-try:
-    while True:
-        dx,dy=d[i]
-        g[y][x][1].add( (dx,dy) )
-        i=(i+1)%4
-        while not g[y+dy][x+dx][0]:
-            if d[i] in g[y][x][1]:
-                o.add( ( x+dx, y+dy ) )
-            x+=dx
-            y+=dy
-            assert x >=0 and y >=0
-            g[y][x][1].add( (dx,dy) )
-except: pass
+dir=( (0,-1 ), ( 1,0 ), ( 0, 1 ), ( -1, 0 ) )
 
-print( sum( ( len( p ) > 0 for l in g for _, p in l ) ) )
-
-for y, line in enumerate( g ):
-    s=""
-    for x, ( w, p ) in enumerate( line ):
-        if w:
-            s+="#"
-        elif (x,y) == start:
-            s+="^"
-        elif (x,y) in o:
-            s+="O"
-        elif len( p ) == 0:
-            s+="."
-        elif set( dx for dx, _ in p ) == set( ( 0, ) ):
-            s+="|"
-        elif set( dy for _, dy in p ) == set( ( 0, ) ):
-            s+="-"
+def path( x,y,i=0 ):
+    p=dict()
+    dx, dy = dir[i]
+    while i not in p.get( (x,y), {} ):
+        p.setdefault( (x,y ) , set() ).add( i )
+        if x in ( 0, w-1 ) or y in ( 0, h-1 ):
+            return p
+        x1, y1 = x+dx, y+dy
+        if (x1,y1) in walls:
+            i=(i+1)%4
+            dx, dy = dir[i]
         else:
-            s+="+"
-    print( s )
+            x, y = x1, y1
+    return None
+
+p = path( *start )
+print( len( p ) )
+
+#part2
+o=set()
+for (x,y),d in p.items():
+    walls.add( (x,y) )
+    if path( *start ) == None:
+        o.add( ( x, y ) )
+    walls.remove( (x,y) )
+
+print( len( o ) )
+
+# for y in range( h ):
+#     s=""
+#     for x in range( w ):
+#         if (x,y) in walls:
+#             s+="#"
+#         elif (x,y) == start:
+#             s+="^"
+#         elif (x,y) in o:
+#             s+="O"
+#         elif (x,y) not in p:
+#             s+="."
+#         elif p[(x,y)].issubset( { 0, 2 } ):
+#             s+="|"
+#         elif p[(x,y)].issubset( { 1, 3 } ):
+#             s+="-"
+#         else:
+#             s+="+"
+#     print( s )
+    
